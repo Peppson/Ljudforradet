1c6773d40337d064c17a7fdcb668827c0374acff
Jesper <jw_jw@hotmail.com>
2025-09-13 17:32:29 +0200

    add hamburger to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { Link, NavLink } from 'react-router-dom';
-import routes from '../routes';
-import { Row, Col } from 'react-bootstrap';
-import Logo from './logo';
+import { Navbar, Container, Nav } from "react-bootstrap";
+import routes from "../routes";
+import Logo from "./logo";
 
 export default function Header() {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [expanded, setExpanded] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -17,37 +18,40 @@ export default function Header() {
   }, []);
 
   return (
-    <header className={isScrolled ? "header scrolled" : "header"}>
-      <Row>
-        <Col>
-          <div className='container'>
-            <div className="d-flex align-items-center justify-content-between">
+    <header className={isScrolled || expanded ? "header scrolled" : "header"}>
+      <Navbar expanded={expanded} expand="md" variant="dark">
+        <Container className="d-flex justify-content-between align-items-center">
 
-              <div className='p-3'>
-                <Link to="/" className="text-decoration-none">
-                  <div className="d-flex">
-                    <div className="pe-2">
-                      <Logo />
-                    </div>
-                    <h2>Ljudförrådet</h2>
-                  </div>
-                </Link>
+          <div className="p-3">
+            <Link to="/" className="text-decoration-none">
+              <div className="d-flex">
+                <div className="pe-2">
+                  <Logo />
+                </div>
+                <h2>Ljudförrådet</h2>
               </div>
-
-              <nav>
-                {routes
-                  .filter(x => x.menuLabel)
-                  .map(({ menuLabel, path }, i) =>
-                    <NavLink
-                      className="p-3 text-decoration-none"
-                      key={i}
-                      to={path}>{menuLabel}
-                    </NavLink>)}
-              </nav>
-            </div>
+            </Link>
           </div>
-        </Col>
-      </Row >
+
+          <Navbar.Toggle onClick={() => setExpanded(!expanded)} />
+
+          <Navbar.Collapse className="justify-content-end">
+            <Nav>
+              {routes.filter(x => x.menuLabel).map(
+                ({ menuLabel, path }, i) =>
+                  <Nav.Link
+                    as={Link}
+                    key={i}
+                    to={path}
+                    className="text-white ps-3"
+                    onClick={() => setTimeout(() => setExpanded(false), 100)}>
+                    {menuLabel}
+                  </Nav.Link>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header >
   );
 }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,3 @@
-import { Col, Row } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 
 export default function Main() {
